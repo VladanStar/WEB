7629b84dad1da46c5a446412215455a2e66bea59
VladanStar <vladancupric@gmail.com>
2021-05-29 16:18:11 +0200

    New task built in windows object

--- BOM/BOM/index.js
+++ BOM/BOM/index.js
@@ -4,9 +4,8 @@ console.log("Hello World again!!!");
   console.log("platform: ", window.navigator);
 })();
 
-
 (function getPlatform() {
-    console.log("platform: ", window.navigator.platform);
+  console.log("platform: ", window.navigator.platform);
 })();
 
 //getPlatform();
@@ -22,7 +21,7 @@ function browserCompany() {
 browserCompany();
 
 function isOnline() {
-  var status ="";
+  var status = "";
   if (window.navigator.onLine === true) {
     status = "status: online";
   } else {
@@ -39,8 +38,8 @@ function windowBrowserRes() {
 windowBrowserRes();
 
 function maxPossibile() {
-    console.log("curren height: ", window.screen.availHeight);
-      console.log("current width: ", window.screen.availWidth);
+  console.log("curren height: ", window.screen.availHeight);
+  console.log("current width: ", window.screen.availWidth);
 }
 maxPossibile();
 
@@ -63,8 +62,8 @@ function protocolName() {
 protocolName();
 
 function parametarsURL() {
-    console.log("parametars one: ", window.location.origin);
-    console.log("parameters two: ", window.location.pathname);
+  console.log("parametars one: ", window.location.origin);
+  console.log("parameters two: ", window.location.pathname);
 }
 parametarsURL();
 
@@ -77,3 +76,98 @@ parametarsURL();
 //     window.location.replace(Vladan);
 // }
 // redirect('https://github.com/VladanStar');
+
+
+function setName() {
+  window.localStorage.setItem("Name", "Vladan");
+  window.localStorage.setItem("Age", "48");
+}
+setName();
+
+
+function getName() {
+  console.log(window.localStorage.getItem("Name"));
+}
+getName();
+
+
+function getAge() {
+  if (window.localStorage.getItem("Age")) {
+    console.log(window.localStorage.getItem("Age"));
+ 
+  } else {
+       console.log("There is no data");
+    
+  }
+}
+getAge();
+
+function clearData() {
+    window.localStorage.clear();
+  }
+clearData();
+ 
+
+ function sessionData() {
+   window.sessionStorage.setItem("Pet", "Cat");
+}
+sessionData();
+
+//************************ */
+ function setSesName() {
+   window.sessionStorage.setItem("Name", "Vladan");
+   window.sessionStorage.setItem("Age", "48");
+ }
+ setSesName();
+/************* */
+ function getSesName() {
+   console.log(window.sessionStorage.getItem("Name"));
+ }
+ getSesName();
+/****** */
+function removeSessInfo(a) {
+ window.sessionStorage.removeItem("Name");
+}
+removeSessInfo("Name");
+
+/********************** */
+function getSessName() {
+  if (window.sessionStorage.getItem("Name")) {
+    console.log(window.sessionStorage.getItem("Name"));
+  } else {
+    console.log("There is no data");
+  }
+}
+getSessName();
+/******************* */
+
+function goBack() {
+  window.history.go(-2);
+}
+goBack();
+//window.history
+
+window.history.forward();
+window.history.back();
+ 
+
+
+
+
+
+
+
+
+//  function greetMsg() {
+//    window.alert("Hello user!");
+//  }
+//  greetMsg();
+
+//  function userQuestion() {
+//    var msg = window.prompt("How old are you?");
+
+//    if (window.confirm("We will provide this answer now " + msg)) {
+//      window.alert("Success");
+//    }
+//  }
+//  userQuestion();
