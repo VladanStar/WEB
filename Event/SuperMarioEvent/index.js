8c2bbb76af092f1a11b9ae58981fb296655832ee
VladanStar <vladancupric@gmail.com>
2021-06-12 17:06:36 +0200

    Commit Event

--- Event/SuperMarioEvent/index.js
+++ Event/SuperMarioEvent/index.js
@@ -1,36 +1,26 @@
-var path = document.querySelector('.path');
-var mario = document.querySelector('.mario');
+var path = document.querySelector(".path");
+var mario = document.querySelector(".mario");
 var body = document.body;
 var x = 0;
 var moveBg;
 
-
-window.addEventListener('keydown', function (e) {
-    
-    if(e.key == 'ArrowRight') {
-        if(mario.getAttribute('src') === './SuperMarioAssets/mario.png') {
-            mario.removeAttribute('src');
-            mario.setAttribute('src', './SuperMarioAssets/mario_running.gif');
-            moveBg = setInterval(function() {
-                
-                x-=1;
-                path.style.backgroundPosition = x + 'px bottom';
-                
-            },1)
-            
-        }
-        
+window.addEventListener("keydown", function (e) {
+  if (e.key == "ArrowRight") {
+    if (mario.getAttribute("src") === "./SuperMarioAssets/mario.png") {
+      mario.removeAttribute("src");
+      mario.setAttribute("src", "./SuperMarioAssets/mario_running.gif");
+      moveBg = setInterval(function () {
+        x -= 1;
+        path.style.backgroundPosition = x + "px bottom";
+      }, 1);
     }
-    
-    
-})
+  }
+});
 
-window.addEventListener('keyup', function (e) {
-    
-    if(e.key == 'ArrowRight') {
-        mario.removeAttribute('src');
-        mario.setAttribute('src', './SuperMarioAssets/mario.png');
-        clearInterval(moveBg);
-    }
-})
-
+window.addEventListener("keyup", function (e) {
+  if (e.key == "ArrowRight") {
+    mario.removeAttribute("src");
+    mario.setAttribute("src", "./SuperMarioAssets/mario.png");
+    clearInterval(moveBg);
+  }
+});
