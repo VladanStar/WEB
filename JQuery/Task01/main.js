bd602742e9c59d93b00e52c826e6ab6df5ee057a
VladanStar <vladancupric@gmail.com>
2021-06-24 08:27:12 +0200

    New commits

--- JQuery/Task01/main.js
+++
@@ -1,6 +0,0 @@
-$(function () {
-    $("li:first").toggleClass('redBorder');
-    $("li").addClass("text");
-    $("li.active").addClass("fontColor");
-    $("li:nth-child(3)").toggleClass("bgcolor");
-});
