f18ffacd067fdd24055ca435acc3464fb7e82b17
unknown <vladancupric@gmail.com>
2021-06-25 12:09:20 +0200

    New commits

--- jQuery normalan jedan jednini/Task04/js/insertEl.js
+++ jQuery normalan jedan jednini/Task04/js/insertEl.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
   var images = [
     "./images/player1.jpg",
     "./images/player2.jpg",
@@ -11,30 +11,38 @@ $(function() {
     "./images/player9.jpg",
     "./images/player10.jpg",
     "./images/player11.jpg",
-    "./images/player12.jpg"
+    "./images/player12.jpg",
   ];
 
   for (var i = 0; i < images.length; i++) {
     var img = $("<img>");
     $("div").append(img);
 
-    $("img").each(function(index, element) {
+    $("img").each(function (index, element) {
       $(element).attr("src", images[index]);
     });
   }
 
   $("div").prepend("<h1>Players</h1>");
 
-  var randomNumb = function() {
+  var randomNumb = function () {
     return Math.floor(200 + Math.random() * 300);
   };
 
   setInterval(
-    $("img").each(function(index, element) {
+    $("img").each(function (index, element) {
       var ran = randomNumb();
       $(element).css("width", ran + "px");
     }),
     1000
   );
-  
+ 
+  var $imgSelected = $("img");
+  $imgSelected.each(function () {
+    if ($(this).width() < 300) {
+      $(this).addClass("border");
+    } else {
+      return true;
+    }
+  });
 });
