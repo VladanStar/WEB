4c22c553e54bda222f3f1ea43178e4d5f5a14ba3
unknown <vladancupric@gmail.com>
2021-06-30 22:40:33 +0200

    New commits

---
+++ AJAX/Task03/js/main.js
@@ -0,0 +1,23 @@
+let $searchInput = $("#search");
+let $resultWrapper = $("#result-wrapper");
+
+$searchInput.keydown(function (e) {
+  if (e.keyCode == 13) {
+    let $subject = $searchInput.val();
+    $.ajax({
+      url: `https://api.github.com/search/users?q=${$subject}+in:user`,
+      type: "GET",
+      dataType: "json",
+    }).done(function (response) {
+      console.log(response);
+      response.items.forEach((element) => {
+        let $responseBox = $("<div>");
+        $responseBox.html(
+          `<div class="img-handler"><img src="${element.avatar_url}"></div><a href="${element.html_url}" target="_blank"><h3>${element.login}</h3></a>`
+        );
+        $resultWrapper.append($responseBox);
+        $responseBox.addClass("response-box");
+      });
+    });
+  }
+});
